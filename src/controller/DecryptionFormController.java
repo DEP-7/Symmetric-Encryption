92c30babc3f7e75a2f5e13ad5d10dd0f3639fa71
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-05-24 10:01:33 +0530

    All the UI created

--- src/controller/DecryptionFormController.java
+++ src/controller/DecryptionFormController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class DecryptionFormController {
+    public JFXTextField txtCipherText;
+    public JFXTextField txtKey;
+    public JFXTextField txtOutput;
+
+    public void btnGoBack_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDecryption_OnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/EncryptionFormController.java
+++ src/controller/EncryptionFormController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class EncryptionFormController {
+    public JFXTextField txtText;
+    public JFXTextField txtKey;
+    public JFXTextField txtOutput;
+
+    public void btnGoBack_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnEncryption_OnAction(ActionEvent actionEvent) {
+    }
 }
