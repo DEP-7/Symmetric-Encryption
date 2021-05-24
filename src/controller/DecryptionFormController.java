12a67f3945a224c03e0cf17932650b058a8ff75b
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-05-24 11:34:33 +0530

    Finished code with new algorithm

--- src/controller/DecryptionFormController.java
+++ src/controller/DecryptionFormController.java
@@ -2,6 +2,8 @@ package controller;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 public class DecryptionFormController {
     public JFXTextField txtCipherText;
@@ -11,21 +13,30 @@ public class DecryptionFormController {
     public void btnDecryption_OnAction(ActionEvent actionEvent) {
         String text = txtCipherText.getText();
         String key = txtKey.getText();
-        System.out.println(text.substring(text.length() - key.length(), text.length()).equals(key));
-        if (text.substring(text.length() - key.length(), text.length()).equals(key)) {
-            txtOutput.setText(getDecryptedValue(text));
+        if (key.trim().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
+            txtKey.requestFocus();
+            return;
+        }
+        //System.out.println(text.substring(text.length() - key.length(), text.length()).equals(key));
+        //if (text.substring(text.length() - key.length(), text.length()).equals(key)) {
+        //    txtOutput.setText(getDecryptedValue(text));
+        //}
+        String output=getDecryptedValue(text);
+        if (output.substring(output.length() - key.length(), output.length()).equals(key)){
+            txtOutput.setText(output.substring(0,output.length()-1));
         }
     }
 
     private String getDecryptedValue(String text) {
-        text = text.substring(0, text.length() - 1);
+        //text = text.substring(0, text.length() - 1);
         //text = text.substring(0, 1) + text.substring(1);
         String output = "";
         for (int i = 0; i < text.length(); i+=3) {
             //System.out.println(Character.toString((char)Integer.parseInt(text.substring(i,i+3))));
             output += Character.toString((char)Integer.parseInt(text.substring(i,i+3)));
         }
-        output = output.substring(0, 1) + output.substring(1);
+        output = output.substring(output.length()-1) + output.substring(0,output.length()-1);
         return output;
     }
 }
--- src/controller/EncryptionFormController.java
+++ src/controller/EncryptionFormController.java
@@ -2,11 +2,37 @@ package controller;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 public class EncryptionFormController {
     public JFXTextField txtText;
     public JFXTextField txtKey;
     public JFXTextField txtOutput;
+
+    /*public void btnEncryption_OnAction(ActionEvent actionEvent) {
+        String text = txtText.getText();
+        String key = txtKey.getText();
+        if (key.trim().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
+            txtKey.requestFocus();
+            return;
+        }
+        text += key;
+        String reversedText = "";
+        for (int i = text.length()-1; i >= 0; i--) {
+            reversedText += text.charAt(i);
+        }
+        String cipherText = "";
+        for (int i = 0; i <reversedText.length(); i++) {
+            int code=reversedText.charAt(i);
+            code+=10;
+            char newChar=(char)code;
+            cipherText+=newChar;
+        }
+        txtOutput.setText(cipherText);
+    }*/
+
 
     public void btnEncryption_OnAction(ActionEvent actionEvent) {
         String text = txtText.getText();
@@ -15,12 +41,12 @@ public class EncryptionFormController {
     }
 
     private String getEncryptedValue(String text, String key) {
-        text = text.substring(1) + text.substring(0, 1);
+        text = text.substring(1) +key+ text.substring(0, 1);
         String output="";
         for (int i = 0; i < text.length(); i++) {
-            //System.out.println(String.format("%03d",(int)text.charAt(i)));
+            System.out.println(String.format("%03d",(int)text.charAt(i)));
             output+=String.format("%03d",(int)text.charAt(i));
         }
-        return output+key;
+        return output;
     }
 }
